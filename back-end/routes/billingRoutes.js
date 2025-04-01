48d0bc59e93f1631b597d841db425b39c3231534
Kelum-Tharindu <155412271+Kelum-Tharindu@users.noreply.github.com>
2025-04-01 09:31:52 +0530

    complete basic-parking

--- back-end/routes/billingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const billingController = require("../controllers/billingController");
-
-// Define routes
-router.post("/entry", billingController.createEntry);
-router.post("/exit", billingController.processExit);
-
-module.exports = router;
