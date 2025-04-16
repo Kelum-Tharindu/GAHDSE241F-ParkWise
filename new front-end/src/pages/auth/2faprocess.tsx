c7aed4afac245d725a30b37696e243086876307a
SanchalaLiyanage <sanchliyanage.info@gmail.com>
2025-04-16 22:25:17 +0530

    updated ui for 2fa

--- new front-end/src/pages/auth/2faprocess.tsx
+++ new front-end/src/pages/auth/2faprocess.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent} from 'react';
 import axios from 'axios';
 import QRCodeForm from '@/components/QRCodeForm';
 import OTPForm from '@/components/OTPForm';
