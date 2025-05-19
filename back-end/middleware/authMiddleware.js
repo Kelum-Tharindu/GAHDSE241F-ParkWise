d8ce4d04897169c9c930b7fdd26b55c183d8038a
Kelum-Tharindu <155412271+Kelum-Tharindu@users.noreply.github.com>
2025-05-19 10:21:11 +0530

    Rename User.js to Usermodel.js

--- back-end/middleware/authMiddleware.js
+++ back-end/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/Usermodel");
 const Landowner = require("../models/LandOwner");
 
 const protect = async (req, res, next) => {
