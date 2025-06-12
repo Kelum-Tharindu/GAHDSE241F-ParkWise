62a6bf7520e9f06399f627a069605a1ca93fc944
Kelum Tharindu Gunasinha <155412271+Kelum-Tharindu@users.noreply.github.com>
2025-06-12 22:15:52 +0530

    Delete back-end directory

--- back-end/test/testGenerateBackupCodes.js
+++
@@ -1,5 +0,0 @@
-const generateBackupCodes = require("../utils/generateBackupCodes");
-
-// Test the function
-const backupCodes = generateBackupCodes();
-console.log("Generated Backup Codes:", backupCodes);
